cd6a1af81c1fc27f6ecc36d1bab38aec7bd9611d
Huang Xuxing <grubbybloodorc@gmail.com>
2018-11-02 10:54:46 +0800

    Add cluster test

---
+++ molecule/cluster/tests/test_cluster.py
@@ -0,0 +1,14 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('candidate')
+
+
+def test_cluster_nodes(host):
+    cmd = host.run('redis-cli cluster nodes')
+
+
+    assert cmd.rc == 0
+    assert len(cmd.stdout.split('\n')) == 6
--- molecule/cluster/tests/test_default.py
+++ molecule/cluster/tests/test_default.py
@@ -3,12 +3,43 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('redis')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_redis_file(host):
+    u = host.user('redis')
+
+    assert u.name == 'redis'
+    assert 'redis' in u.groups
+
+
+def test_redis_conf_file(host):
+    f = host.file('/etc/redis/6379.conf')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'redis'
+
+
+def test_redis_nodes_file(host):
+    f = host.file('/etc/redis/nodes.conf')
+
+    assert f.exists
+
+
+def test_redis_init_file(host):
+    f = host.file('/etc/init.d/redis-6379')
+
+    assert f.exists
+
+
+def test_redis_pid_file(host):
+    f = host.file('/var/run/redis/6379.pid')
+
+    assert f.exists
+
+
+def test_redis_service(host):
+    s = host.service('redis-6379')
+
+    assert s.is_enabled
+    assert s.is_running
